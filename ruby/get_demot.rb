878140726587c82e20f243d77b5be3808793b08a
gondar <marcin@bazydlo.org>
2010-04-26 12:57:51 +0200

    Pobieranie obrazkow za pomoca wget.

--- ruby/get_demot.rb
+++ ruby/get_demot.rb
@@ -3,4 +3,7 @@ require 'httpclient' #Nasza przegladarka www
 
 c = HTTPClient.new #Nowy obiekt
 page = c.get('http://demotywatory.pl').content #pobieramy stronke
-puts page.scan(/<img src="(.*)" class="demot"/).flatten.inspect #szukamy obrazkow
+pictures = page.scan(/<img src="(.*)" class="demot"/).flatten #szukamy obrazkow
+pictures.each do |p|
+  `wget #{p}`
+end
