e54f858566108ae437ad6b72291c9f91491c5467
gondar <marcin@bazydlo.org>
2010-04-29 16:13:31 +0200

    Rails HW

--- szydera/config/routes.rb
+++ szydera/config/routes.rb
@@ -1,4 +1,8 @@
 Szydera::Application.routes.draw do |map|
+  get "home/index"
+
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
