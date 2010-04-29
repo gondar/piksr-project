139721e38ee52021c12eb77da2f27aab6b46ec2b
gondar <marcin@bazydlo.org>
2010-04-29 15:50:25 +0200

    Wygenerowana aplikacja

---
+++ szydera/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'e104f4d216dbde1f34b86fa7764bfbf768c8e1456864d7e286704a86f28da33b5adfc37cab93bff2e701f5c79ffc6ff7a6424c58601bbdf0dc9a6902ccbd4420'
